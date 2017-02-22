0db78742b0d4c7926e9f0c3330b7ae87c2425486
M4nni <finnwestphal98@gmail.com>
2017-02-22 18:13:00 +0100

    Add files via upload

---
+++ greenfoot space 18.09.2016/Manni.java
@@ -0,0 +1,28 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * Write a description of class Manni here.
+ */
+public class Manni
+{
+    /* Instance variables - replace the example below with your own*/
+    private int x;
+
+    /**
+     * Constructor for objects of class Manni
+     */
+    public Manni()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     */
+    public int sampleMethod(int y)
+    {
+        /* Put your code here*/
+        return x + y;
+    }
+}
